f8d4bb44151d2ba4a01e3042e7fb8b49b522b179
MayerEmad <mayer.emad@yahoo.com>
2020-05-07 02:28:48 +0200

    Create shipment item

---
+++ app/src/main/java/com/example/flyshippment_project/CreateShipmentItemActivity.java
@@ -0,0 +1,12 @@
+package com.example.flyshippment_project;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateShipmentItemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
--- app/src/main/java/com/example/flyshippment_project/ShipmentNavFragment.java
+++ app/src/main/java/com/example/flyshippment_project/ShipmentNavFragment.java
@@ -14,7 +14,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 import adapters_and_items.AdapterRecyclerShipment;
 import adapters_and_items.Repository;
@@ -25,12 +29,10 @@ import adapters_and_items.SearchViewModel;
 public class ShipmentNavFragment extends Fragment {
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter ;
 
     public ShipmentNavFragment() {
         // Required empty public constructor
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -40,16 +42,22 @@ public class ShipmentNavFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
-        final SearchViewModel viewModel = ViewModelProviders.of(getActivity()).get(SearchViewModel.class);
+        final SearchViewModel viewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(SearchViewModel.class);
+        FloatingActionButton fab =(FloatingActionButton) view.findViewById(R.id.floating_button_shipment_nav);
         recyclerView = (RecyclerView) view.findViewById(R.id.user_shipments_recycler_view);
 
         //Intialise the Recycler Viewer
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
-        mAdapter=new AdapterRecyclerShipment(Repository.getShipmentsFromApi(),getContext());
+        RecyclerView.Adapter mAdapter = new AdapterRecyclerShipment(Repository.getShipmentsFromApi(), getContext());
         recyclerView.setAdapter(mAdapter);
 
-
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
         //When the LiveData  Changes due to Loading or Filtering it will be updated here
         viewModel.getShipmentLiveData().observe(getViewLifecycleOwner(), new Observer<ArrayList<ShipmentItem>>() {
             @Override
